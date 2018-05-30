ff35ec4c966aa9a5a5607eb606e7659a60930672
Dylan Nichols <9994086@student.toiohomai.ac.nz>
2018-05-31 10:32:48 +1200

    initial commit

---
+++ assignment-practice/Message.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignment_practice
+{
+    class Message
+    {
+        public string MessageSender { get; set; }
+        public string MessageBody { get; set; }
+        public bool MessageStatus { get; set; }
+    }
+}
